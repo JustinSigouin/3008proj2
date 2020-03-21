9f8085bf8fa64eb40e4aef626cc9a0b370297de6
Justin Sigouin <justin9@hotmail.ca>
2020-03-21 14:37:49 -0400

    add readme, update script

--- calculateStatistics.R
+++ calculateStatistics.R
@@ -1,5 +1,6 @@
-output_file <- "output.txt";
-data <- read.table("combined-csv-template.csv", header = TRUE, sep = ","); # read csv file
+# don't forget to set working directory using `setwd("DIR")`
+output_file <- "output.txt"; # output file for stats
+data <- read.table("combined-csv-template.csv", header = TRUE, sep = ","); # read csv file, ignore the column names
 
 # format is userId passwordScheme totalLogins successfulLogins unsuccessfulLogins avgSuccessLoginTime avgFailedLoginTime
 
@@ -8,4 +9,11 @@ full_data <- split(data, data$passwordScheme);
 text_data = full_data$Text21;
 image_data = full_data$Image21;
 
-meanTotalUserLogins <- mean(text_data$totalLogins);
+
+calculateStatistics <- function(data, scheme) {
+	meanTotalUserLogins <- mean(data$totalLogins);
+	cat(paste("Mean total logins for: ", scheme, meanTotalUserLogins), "\n");
+}
+
+calculateStatistics(text_data, "Text21");
+calculateStatistics(image_data, "Image21");
