883d7eb2db70cd6d715923a0d103b03089115425
Justin Sigouin <justin9@hotmail.ca>
2020-03-22 00:28:08 -0400

    finish plots

--- calculateStatistics.R
+++ calculateStatistics.R
@@ -11,9 +11,11 @@ image_data = full_data$Image21;
 
 
 calculateStatistics <- function(data, scheme) {
+	sink(output_file);
 	calculateLoginStats(data, scheme);
 	calculateTimeStats(data, scheme);
-	pdf(file = output_file);
+	sink();
+	pdf(file = paste(scheme, output_file));
 	createLoginHistograms(data, scheme);
 	createTimeHistograms(data, scheme);
 	dev.off();
@@ -23,10 +25,8 @@ calculateLoginStats <- function(data, scheme) {
 	cat(paste("Mean for logins for", scheme),"\n");
 	meanTotalUserLogins <- mean(data$totalLogins);
 	cat(paste("Mean total logins for", scheme, meanTotalUserLogins), "\n");
-	
 	meanSuccessfulUserLogins <- mean(data$successfulLogins);
 	cat(paste("Mean successful logins for", scheme, meanSuccessfulUserLogins), "\n");
-	
 	meanUnsuccessfulUserLogins <- mean(data$unsuccessfulLogins);
 	cat(paste("Mean unsuccessful logins for", scheme, meanUnsuccessfulUserLogins), "\n");
 	cat("\n");
@@ -56,38 +56,46 @@ calculateLoginStats <- function(data, scheme) {
 
 calculateTimeStats <- function(data, scheme) {
 	cat(paste("Mean for login time for for", scheme),"\n");
-	meanSuccessfulUserLoginsTime <- mean(data$avgSuccessLoginTime);
+	meanSuccessfulUserLoginsTime <- mean(data$avgSuccessLoginTime[data$avgSuccessLoginTime > 0]);
 	cat(paste("Mean successful login time for", scheme, meanSuccessfulUserLoginsTime), "\n");
 	
-	meanUnsuccessfulUserLoginTime <- mean(data$avgFailedLoginTime);
+	meanUnsuccessfulUserLoginTime <- mean(data$avgFailedLoginTime[data$avgFailedLoginTime > 0]);
 	cat(paste("Mean unsuccessful login time for", scheme, meanUnsuccessfulUserLoginTime), "\n");
 	cat("\n");
 	
 	cat(paste("Standard deviation for login time for", scheme),"\n");
-	sdSuccessfulUserLoginTime <- sd(data$avgSuccessLoginTime);
+	sdSuccessfulUserLoginTime <- sd(data$avgSuccessLoginTime[data$avgSuccessLoginTime > 0]);
 	cat(paste("Standard deviation of successful login time for", scheme, sdSuccessfulUserLoginTime), "\n");
 	
-	sdUnsuccessfulUserLoginTime <- sd(data$avgFailedLoginTime);
+	sdUnsuccessfulUserLoginTime <- sd(data$avgFailedLoginTime[data$avgFailedLoginTime > 0]);
 	cat(paste("Standard deviation of unsuccessful login time for", scheme, sdUnsuccessfulUserLoginTime), "\n");
 	cat("\n");
 	
 	cat(paste("Median for login time for", scheme),"\n");
-	medianSuccessfulUserLoginTime <- median(data$avgSuccessLoginTime);
+	medianSuccessfulUserLoginTime <- median(data$avgSuccessLoginTime[data$avgSuccessLoginTime > 0]);
 	cat(paste("Median successful login time for", scheme, medianSuccessfulUserLoginTime), "\n");
 	
-	medianUnsuccessfulUserLoginTime <- median(data$avgFailedLoginTime);
+	medianUnsuccessfulUserLoginTime <- median(data$avgFailedLoginTime[data$avgFailedLoginTime > 0]);
 	cat(paste("Median unsuccessful login time for", scheme, medianUnsuccessfulUserLoginTime), "\n");
 	cat("\n");
 }
 
 createLoginHistograms <- function(data, scheme) {
-	hist(data$totalLogins,main=paste(scheme, "Total Logins"),xlab="Number of Logins", col="darkmagenta");
-	hist(data$successfulLogins,main=paste(scheme, "Successful Logins"),xlab="Successful Logins", col="green");
-	hist(data$unsuccessfulLogins,main=paste(scheme, "Unsuccessful Logins"),xlab="Unsuccessful Logins", col="lightblue");
+	hist(data$totalLogins,main=paste(scheme, "Total Logins"), breaks=seq(0, 20, l=20), xlab="Number of Logins", col="darkmagenta");
+	hist(data$successfulLogins[data$successfulLogins > 0],main=paste(scheme, "Successful Logins"), breaks=seq(0, 10, l=10), xlab="Successful Logins", col="green");
+	hist(data$unsuccessfulLogins[data$unsuccessfulLogins > 0],main=paste(scheme, "Unsuccessful Logins"), breaks=seq(0, 10, l=10), xlab="Unsuccessful Logins", col="lightblue");
 }
 
 createTimeHistograms <- function(data, scheme) {
+	b <- NULL;
+	ifelse(scheme == "Text21:", b <- seq(0, 20, l=20), b <- seq(0, 40, l=20));
+	hist(data$avgSuccessLoginTime[data$avgSuccessLoginTime > 0], main=paste(scheme, "Average Successful Login Time"), b, xlab="Successful Login Time", col="darkmagenta");
+	ifelse(scheme == "Text21:", b <- seq(0, 30, l=30), b <- seq(0, 60, l=20));
+	hist(data$avgFailedLoginTime[data$avgFailedLoginTime > 0], main=paste(scheme, "Average Unsuccessful Login Time"), b,  xlab="Unsuccessful Login Time", col="green");
 	
+	boxplot(data$avgSuccessLoginTime[data$avgSuccessLoginTime > 0], main=paste(scheme, "Box Plot Of Average Successful Login Time"), xlab="Successful Login Time");
+	boxplot(data$avgFailedLoginTime[data$avgFailedLoginTime > 0], main=paste(scheme, "Box Plot Of Average Unsuccessful Login Time"), xlab="Unsuccessful Login Time");
+
 }
 
 calculateStatistics(text_data, "Text21:");
